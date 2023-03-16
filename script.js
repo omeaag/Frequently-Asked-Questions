149e26873d2218c6989e92b7385df0c8232147cf
can <omercansoyu@gmail.com>
2023-03-16 12:58:19 +0300

    project is over

---
+++ script.js
@@ -0,0 +1,9 @@
+let contentContainer = document.getElementsByClassName('content-container');
+
+for(let i = 0; i < contentContainer.length; i++){
+    contentContainer[i].addEventListener('click',function(){
+        this.classList.toggle('active');
+    })
+
+    console.log(contentContainer[i]);
+}
